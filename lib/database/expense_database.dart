c4150b4d0b6e93c69bc344eb91768e4ed60a5d21
erickshayo <erickshayo306@gmail.com>
2024-02-21 22:29:48 +0300

    first commit

---
+++ lib/database/expense_database.dart
@@ -0,0 +1,36 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class ExpenseDatabase extends ChangeNotifier {
+  static late Isar isar;
+  List<Expense> _allExpenses = [];
+
+  /*
+  S E T U P
+  
+  */
+
+  //initiallize the database
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open([ExpenseSchema], directory: dir.path);
+  }
+
+  /*
+  GETTERS
+  
+  */
+
+/*
+
+  OPERATIONS
+  
+  */
+
+/*
+  HELPERS
+  
+  */
+}
