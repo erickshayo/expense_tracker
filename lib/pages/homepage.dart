c296adaef6d98066747e952fa60bec3656bb7218
erickshayo <erickshayo306@gmail.com>
2024-02-23 00:13:22 +0300

    first commit

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+TextEditingController nameController = TextEditingController();
+TextEditingController amountController = TextEditingController();
+
+class _HomePageState extends State<HomePage> {
+  void openNewExpenseBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("New expense"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(hintText: "Name"),
+            ),
+            TextField(
+              controller: amountController,
+              decoration: InputDecoration(hintText: "Amount"),
+            )
+          ],
+        ),
+        actions: [
+          _cancelButton()
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: openNewExpenseBox,
         child: Icon(Icons.add),
       ),
     );
   }
+}
+
+Widget _cancelButton() {
+  return MaterialButton(
+    onPressed: () {
+
+
+
+    },
+    child: Text("Cancel"),
+  );
 }
