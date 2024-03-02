8a7704e0d6ca1e652b5b3956295b336832cec1e8
erickshayo <erickshayo306@gmail.com>
2024-03-02 13:21:42 +0300

    first commit

--- lib/bar_graph/bar_graph.dart
+++ lib/bar_graph/bar_graph.dart
@@ -34,7 +34,78 @@ class _MyBarGraphState extends State<MyBarGraph> {
       BarChartData(
         minY: 0,
         maxY: 100,
-      )
+        gridData: FlGridData(show: false),
+        borderData: FlBorderData(show: false),
+        titlesData: FlTitlesData(
+          show: true,
+          topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: getBottomTitles,
+            ),
+          ),
+        ),
+      ),
     );
   }
+}
+
+Widget getBottomTitles(double value, TitleMeta meta) { 
+  const textstyle = TextStyle(
+    color: Colors.grey,
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+  );
+
+  String text;
+  switch (value.toInt()) {
+    case 0:
+      text = 'J';
+      break;
+    case 1:
+      text = 'F';
+      break;
+    case 2:
+      text = 'M';
+      break;
+    case 3:
+      text = 'A';
+      break;
+    case 4:
+      text = 'M';
+      break;
+    case 5:
+      text = 'J';
+      break;
+    case 6:
+      text = 'J';
+      break;
+    case 7:
+      text = 'A';
+      break;
+    case 8:
+      text = 'S';
+      break;
+    case 9:
+      text = 'O';
+      break;
+    case 10:
+      text = 'N';
+      break;
+    case 11:
+      text = 'D';
+      break;
+    default:
+      text = '';
+      break;
+  }
+  return SideTitleWidget(
+      child: Text(
+        text,
+        style: textstyle,
+      ),
+      axisSide: meta.axisSide);
 }
